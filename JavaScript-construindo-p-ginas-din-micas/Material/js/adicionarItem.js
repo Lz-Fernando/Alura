bb7b0890557846fdced7af0523627adca6c38513
Fernando Ferreira <168456690+Lz-Fernando@users.noreply.github.com>
2024-10-10 22:02:03 -0300

    Add files via upload

---
+++ JavaScript-construindo-p-ginas-din-micas/Material/js/adicionarItem.js
@@ -0,0 +1,13 @@
+import { criarItemDaLista } from "./criandoItemDaLista.js";
+import { verificarListaVazia } from "./verificarListaVazia.js";
+
+const item = document.getElementById("input-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
+
+ export function adicionarItem(evento) {
+    evento.preventDefault();
+
+    const itemDaLista = criarItemDaLista(item.value)
+    listaDeCompras.appendChild(itemDaLista);
+    verificarListaVazia(listaDeCompras);
+}

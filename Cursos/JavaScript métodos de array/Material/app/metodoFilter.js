e0927d7d1556134f068771914948ba427f2ca675
Fernando Ferreira <nandobritoferreira@hotmail.com>
2024-10-22 08:51:50 -0300

    Filter de livros

--- Cursos/JavaScript métodos de array/Material/app/metodoFilter.js
+++ Cursos/JavaScript métodos de array/Material/app/metodoFilter.js
@@ -1,8 +1,9 @@
-const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront');
+const botoes = document.querySelectorAll('.btn');
 
-btnFiltrarLivrosDeFront.addEventListener('click', filtrarLivrosDeFront);
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
 
-function filtrarLivrosDeFront() {
-    let livrosFiltrados = livros.filter(livro => livro.categoria == 'front-end')
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id);
+    const categoria = elementoBtn.value
+    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
     console.table(livrosFiltrados)
-}

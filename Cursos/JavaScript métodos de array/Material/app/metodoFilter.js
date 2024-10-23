dde5f510df6ed3c5539a3f447f2c57ae2723fe40
Fernando Ferreira <nandobritoferreira@hotmail.com>
2024-10-23 09:08:42 -0300

    Manipulando HTML com JS

--- Cursos/JavaScript métodos de array/Material/app/metodoFilter.js
+++ Cursos/JavaScript métodos de array/Material/app/metodoFilter.js
@@ -5,6 +5,25 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id);
     const categoria = elementoBtn.value;
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);
+    let livrosFiltrados = categoria == 'disponivel' ? verificarDisponibilidade() : verificarCategoria(categoria);
     exibirOsLivrosNaTela(livrosFiltrados);
+    if (categoria == 'disponivel') {
+        exibirValorTotalDosLivrosDisponiveisNaTela()
+    }
+}
+
+function verificarCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+}
+
+function verificarDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela () {
+    elementoComValorTotalDeLivrosDisponveis.innerHTML = `
+    <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+    </div>
+    `
 }

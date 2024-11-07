0d6f3fb00b2d9082182f251f70fd565d87ef5624
Fernando Ferreira <nandobritoferreira@hotmail.com>
2024-11-07 19:17:58 -0300

    loops e tentativas

---
+++ Cursos/Lógica de programação mergulhe em programação com JavaScript/Desafios/atv3.js
@@ -0,0 +1,30 @@
+//1
+let contador = 1;
+while (contador <= 10) {
+    console.log(contador);
+    contador++;
+}
+
+//2
+let contador = 10;
+while (contador >= 0) {
+    console.log(contador);
+    contador--;
+}
+
+//3
+let numeroMaximo = prompt("Digite um número para a contagem regressiva:");
+
+while (numeroMaximo >= 0) {
+    console.log(numeroMaximo);
+    numeroMaximo--;
+}
+
+//4
+let numeroMaximo = prompt("Digite um número para a contagem progregressiva:");;
+let contador = 0;
+
+while (contador <= numeroMaximo) {
+    console.log(contador);
+    contador++
+}

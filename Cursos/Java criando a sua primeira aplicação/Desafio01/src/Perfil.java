7b053177d0e26a77fd249cf5cfe73819de84eca0
Fernando Ferreira <nandobritoferreira@hotmail.com>
2024-11-11 20:07:33 -0300

    Desafio: hora da prática

---
+++ Cursos/Java criando a sua primeira aplicação/Desafio01/src/Perfil.java
@@ -0,0 +1,7 @@
+public class Perfil {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá, Luiz Fernando!");
+
+        System.out.println("Tudo bem?");
+    }
+}

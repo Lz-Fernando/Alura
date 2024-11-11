2198eb568cec07bc1e99151a6fb993e4fde02a68
Fernando Ferreira <nandobritoferreira@hotmail.com>
2024-11-11 20:19:35 -0300

    Atribuindo valores ao filme

--- Cursos/Java criando a sua primeira aplicação/programa/src/App.java
+++ Cursos/Java criando a sua primeira aplicação/programa/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô, Mundo!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
     }
 }

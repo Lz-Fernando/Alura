43dc59a110a1b9e41f449216fc29b4b31834afc4
Fernando Ferreira <nandobritoferreira@hotmail.com>
2024-11-11 20:37:47 -0300

    Convertendo valores

--- Cursos/Java criando a sua primeira aplicação/programa/src/App.java
+++ Cursos/Java criando a sua primeira aplicação/programa/src/App.java
@@ -19,5 +19,8 @@ public class App {
                 Ano de lançamento
                 """ + anoDeLancamento;
         System.out.println(sinopse);
+
+        int classificacao = (int) (media / 2);
+        System.out.println(classificacao);
     }
 }

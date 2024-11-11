53adc95a372449d194a4ba934635241aa982af32
Fernando Ferreira <nandobritoferreira@hotmail.com>
2024-11-11 20:46:41 -0300

    Faça como eu fiz: conversão de temperaturas - Corrigido

--- Cursos/Java criando a sua primeira aplicação/faca como eu fiz2/src/ConverterTemperatura.java
+++ Cursos/Java criando a sua primeira aplicação/faca como eu fiz2/src/ConverterTemperatura.java
@@ -1,9 +1,13 @@
 public class ConverterTemperatura {
     public static void main(String[] args) throws Exception {
-        int temperatura = 10;
+        double temperaturaEmCelsius = 30.4;
+        double temperaturaEmFahrenheit = (temperaturaEmCelsius * 1.8) + 32;
 
-        double converter = (int) (temperatura * 1.8) + 32;
+        String mensagem = String.format("A temperatura de %f Celsius é equivalente a %f Fahrenheit", temperaturaEmCelsius, temperaturaEmFahrenheit);
 
-        System.out.println(converter);
+        System.out.println(mensagem);
+
+        int temperaturaEmFahrenheitInteira = (int) temperaturaEmFahrenheit;
+        System.out.println("A temperatura em Fahrenheit inteira é: " + temperaturaEmFahrenheitInteira);
     }
 }

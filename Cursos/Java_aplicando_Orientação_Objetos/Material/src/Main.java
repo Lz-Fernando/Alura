fec88af8c5cbbd1375f0c86f7a085388f6c83233
Fernando Ferreira <nandobritoferreira@hotmail.com>
2024-11-19 17:37:15 -0300

    Faça como eu fiz: projeto com orientação a objetos

--- Cursos/Java_aplicando_Orientação_Objetos/Material/src/Main.java
+++ Cursos/Java_aplicando_Orientação_Objetos/Material/src/Main.java
@@ -1,16 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         Movie meuFilme = new Movie();
+
         meuFilme.nome = "O poderoso chefão";
         meuFilme.anoLancamento = 1978;
         meuFilme.duracaoMinutos = 180;
+        meuFilme.incluidoNoPlano = true;
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(10);
         meuFilme.avalia(5);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalAvaliacoes);
-        System.out.println(meuFilme.pegaMedia());
+
+        System.out.println("Média de avaliações do filme: " + meuFilme.pegaMedia());
     }
 }
--- Cursos/Java_aplicando_Orientação_Objetos/Material/src/Movie.java
+++ Cursos/Java_aplicando_Orientação_Objetos/Material/src/Movie.java
@@ -9,6 +9,8 @@ public class Movie {
     void exibeFichaTecnica () {
         System.out.println("Nome do filme: " + nome);
         System.out.println("Ano de lançamento: " + anoLancamento);
+        System.out.println("Duração em minutos: " + duracaoMinutos);
+        System.out.println("Incluido no plano: " + incluidoNoPlano);
     }
 
     void avalia (double nota) {

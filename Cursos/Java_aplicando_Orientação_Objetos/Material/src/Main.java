6f15e8dcc788e4321bb36761e07917c3498ca82a
Fernando Ferreira <nandobritoferreira@hotmail.com>
2024-11-26 12:17:43 -0300

    Aproveitando o modelo para séries

--- Cursos/Java_aplicando_Orientação_Objetos/Material/src/Main.java
+++ Cursos/Java_aplicando_Orientação_Objetos/Material/src/Main.java
@@ -1,4 +1,5 @@
 import br.com.alura.screenmatch.modelos.Movie;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,5 +16,11 @@ public class Main {
 
         System.out.println("Total de avaliações: " + meuFilme.getTotalAvaliacoes());
         System.out.println("Média de avaliações do filme: " + meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
     }
 }

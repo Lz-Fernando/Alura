05962bfd515a940ebcf22eaf33cd0288addc9938
Fernando Ferreira <nandobritoferreira@hotmail.com>
2024-12-18 10:11:48 -0300

    Instanciando músicas e podcasts

---
+++ Cursos/Java_aplicando_Orientação_Objetos/DesafioFinal/src/br/com/alura/minhasmusicas/principal/Main.java
@@ -0,0 +1,32 @@
+package br.com.alura.minhasmusicas.principal;
+
+import br.com.alura.minhasmusicas.modelos.musica;
+import br.com.alura.minhasmusicas.modelos.podcast;
+
+public class Main {
+    public static void main(String[] args) {
+        musica minhaMusica = new musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        podcast meuPodcast = new podcast();
+        meuPodcast.setTitulo("Bolha Dev");
+        meuPodcast.setApresentador("Luiz Fernando");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+    }
+}

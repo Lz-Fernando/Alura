9085cfdee158e5f2f560a2007ae33c163b21017d
Fernando Ferreira <nandobritoferreira@hotmail.com>
2025-02-12 13:10:24 -0300

    Guardando o número secreto

--- Cursos/C conhecendo a Linguagem das Linguagens/Programa.c
+++ Cursos/C conhecendo a Linguagem das Linguagens/Programa.c
@@ -6,7 +6,10 @@ int main () {
 	printf("*****************************************\n");
 	printf("* Bem vindo ao nosso jogo de adivinhação *\n");
 	printf("*****************************************\n");
-	
+
+	int numeroSecreto = 42;
+
+	printf("O número %d é o secreto\n", numeroSecreto);
 
 	return 0;	
 }

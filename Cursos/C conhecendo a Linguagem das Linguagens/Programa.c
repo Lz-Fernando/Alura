3be7db33c08bb3751d00753dc75fbd081bac85d6
Fernando Ferreira <nandobritoferreira@hotmail.com>
2025-02-17 12:37:21 -0300

    Acrescentando níveis de dificuldade

--- Cursos/C conhecendo a Linguagem das Linguagens/Programa.c
+++ Cursos/C conhecendo a Linguagem das Linguagens/Programa.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define NUMERO_DE_TENTATIVAS 5
 
@@ -14,18 +15,47 @@ int main () {
 	srand(segundos);
 	int numeroGrande = rand();
 	int numeroSecreto = numeroGrande % 100;
+
 	int chute;
 	int tentativas = 1;
 	double pontos = 1000;
 
-	while(1) {
-	//for (int i = 1; i <= NUMERO_DE_TENTATIVAS; i++) {
+	int nivel;
+	printf("Qual o nível de dificuldade?\n");
+	printf("(1) Fácil / (2) Médio / (3) Difícil\n\n");
+	printf("Escolha: ");
+	scanf("%d", &nivel);
+
+	int numeroTentativas;
+	switch (nivel) {
+		case 1:
+			numeroTentativas = 20;
+			break
+		case 2:
+			numeroTentativas = 10;
+			break
+		default:
+			numeroTentativas = 5;
+			break
+	}
+
+	/*if (nivel = 1) {
+		numeroTentativas = 20;
+	} else if (nivel = 2) {
+		numeroTentativas = 10;
+	} else {
+		numeroTentativas = 5;
+	}*/
+
+	int acertou = 0;
+
+	for (int i = 1; i <= numeroTentativas; i++) {
 		printf("Tentativa %d", tentativas);
 		printf("Chute um número: ");
 		scanf("%d", &chute);
 		printf("Seu chute foi %d!\n", chute);
 
-		int acertou = (chute == numeroSecreto);
+		acertou = (chute == numeroSecreto);
 		int maior = chute > numeroSecreto;
 
 		if(chute < 0) {
@@ -34,7 +64,6 @@ int main () {
 		}
 		
 		if (acertou) {
-			printf("Parabéns, você acertou!\n");
 			break;
 		} else if (maior){
 			printf("Seu chute é maior que o número secreto\n");
@@ -47,9 +76,15 @@ int main () {
 		pontos = pontos - pontosPerdidos;
 	}
 
+	if (acertou) {
+		printf("Parabéns, você acertou!\n");
+		printf("Você acertou em %d tentativas!\n", tentativas);
+		printf("Total de pontos:%.1f \n", pontos);
+	} else {
+		printf("Você perdeu! Tente de novo\n");
+	}
+
 	printf("Fim de jogo!\n");
-	printf("Você acertou em %d tentativas!\n", tentativas);
-	printf("Total de pontos:%.1f \n", pontos);
 
 	return 0;
 }

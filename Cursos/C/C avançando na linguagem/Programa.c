9076f21482ecbc7bdc5f4a63e479ca5154150688
Fernando Ferreira <nandobritoferreira@hotmail.com>
2025-02-18 10:37:26 -0300

    Percorrendo o array

--- Cursos/C/C avançando na linguagem/Programa.c
+++ Cursos/C/C avançando na linguagem/Programa.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
@@ -9,6 +10,14 @@ int main()
 	int acertou, enforcou = 0;
 
 	do {
+		char chute;
+		scanf("%c", &chute);
+
+		for (int i = 0; i < strlen(palavraSecreta); i++) {
+			if (palavraSecreta[i] == chute) {
+				printf("A posição %d tem essa letra!\n", i);
+			}
+		}
 
 	} while (!acertou && !enforcou);
 

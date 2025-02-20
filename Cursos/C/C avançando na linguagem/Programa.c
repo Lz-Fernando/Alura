39140bb9c354198c5545bbdea3365ede66d0355f
Fernando Ferreira <nandobritoferreira@hotmail.com>
2025-02-20 11:38:05 -0300

    Contexto das variáveis

--- Cursos/C/C avançando na linguagem/Programa.c
+++ Cursos/C/C avançando na linguagem/Programa.c
@@ -12,7 +12,6 @@ void chuta(char chutes[26], int tentativas) {
 	scanf(" %c", &chute);
 
 	chutes[tentativas] = chute;
-	tentativas++;
 }
 
 int main()
@@ -46,25 +45,11 @@ int main()
 		printf("\n");
 
 		chuta(chutes, tentativas);
+		tentativas++;
 
 	} while (!acertou && !enforcou);
 
 	printf("%s", palavraSecreta);
-
-	/*palavraSecreta[0] = "M";
-	palavraSecreta[1] = "E";
-	palavraSecreta[2] = "L";
-	palavraSecreta[3] = "A";
-	palavraSecreta[4] = "N";
-	palavraSecreta[5] = "C";
-	palavraSecreta[6] = "I";
-	palavraSecreta[7] = "A";
-	palavraSecreta[8] = "\0";
-
-	printf("%c%c%c%c%c%c%c%c\n", palavraSecreta[0],
-		palavraSecreta[1], palavraSecreta[2], palavraSecreta[3],
-		palavraSecreta[4], palavraSecreta[5], palavraSecreta[6],
-		palavraSecreta[7] );*/
 
 	return 0;
 }

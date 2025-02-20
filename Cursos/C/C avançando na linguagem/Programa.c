71166626c47442f6245ee94cf6836d554deca97d
Fernando Ferreira <nandobritoferreira@hotmail.com>
2025-02-20 12:24:37 -0300

    Melhorando a função chuta()

--- Cursos/C/C avançando na linguagem/Programa.c
+++ Cursos/C/C avançando na linguagem/Programa.c
@@ -7,11 +7,12 @@ void abertura() {
     printf("/****************/\n\n");
 }
 
-void chuta(char chutes[26], int tentativas) {
+void chuta(char chutes[26], int* tentativas) {
 	char chute;
 	scanf(" %c", &chute);
 
-	chutes[tentativas] = chute;
+	chutes[(*tentativas)] = chute;
+	(*tentativas)++;
 }
 
 int main()
@@ -44,8 +45,7 @@ int main()
 		}
 		printf("\n");
 
-		chuta(chutes, tentativas);
-		tentativas++;
+		chuta(chutes, &tentativas);
 
 	} while (!acertou && !enforcou);
 

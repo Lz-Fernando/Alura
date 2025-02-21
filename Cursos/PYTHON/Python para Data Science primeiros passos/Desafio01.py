09b8ea1315efa2aa2a398fd00cf47bf9442821f8
Fernando Ferreira <nandobritoferreira@hotmail.com>
2025-02-21 19:38:30 -0300

    Começando com python

---
+++ Cursos/PYTHON/Python para Data Science primeiros passos/Desafio01.py
@@ -0,0 +1,14 @@
+#1)
+print("Escola de Dados da Alura!\n")
+
+#2)
+print("Nome: Luiz Fernando\nSobrenome: Brito Ferreira\n")
+
+#3)
+print("M\nI\nR\nL\nA\n")
+
+#4)
+print(13, "Abril", 2003, "\n")
+
+#5)
+print("Ano atual:", 2025, "\n")

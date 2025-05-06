d8a809372bcabd07fb3f2b167beb67561e54e560
Fernando Ferreira <nandobritoferreira@hotmail.com>
2025-05-05 22:10:51 -0300

    Formulário de alteração

--- Cursos/JAVA/Java Servlet programação web Java/gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ Cursos/JAVA/Java Servlet programação web Java/gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -1,11 +1,13 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<>();
+	private static Integer chaveSequencial = 1;
 	
 	static {
 		Empresa empresa = new Empresa();
@@ -18,10 +20,32 @@ public class Banco {
 	
 	public void adiciona(Empresa empresa) {
 		// TODO Auto-generated method stub
-		lista.add(empresa);
+		empresa.setId(Banco.chaveSequencial++);
+		Banco.lista.add(empresa);
 	}
 	
 	public List<Empresa> getEmpresa(){
 		return Banco.lista;
+	}
+	
+	public void removeEmpresa(Integer id) {
+		Iterator<Empresa> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			
+			if(emp.getId() == id) {
+				it.remove();
+			}
+		}
+	}
+	
+	public Empresa buscaEmpresaPelaId(Integer id) {
+		for(Empresa empresa: lista) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
 	}
 }
